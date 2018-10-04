0296600fd8a4fe29b374165eab76d0983ca8a715
106021055 <32056108+106021055@users.noreply.github.com>
2018-10-04 22:57:39 +0800

    Add files via upload

--- encrypt.java
+++ encrypt.java
@@ -1,11 +1,15 @@
+package homworklogin;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class encrypt extends JFrame {
-    private Container cp;
-    private JPanel jpl1 = new JPanel(new GridLayout(9,1,1,1));
-    private JPanel jpl2 = new JPanel(new GridLayout(1,1,1,1));
-    private JPanel jpl3 = new JPanel(new GridLayout(1,1,1,1));
+    public static void main(String[] args) {
+      //  new encrypt().setVisible(true);
+    }
+    Container cp;
     private JTextArea jtaL = new JTextArea("");
     private JTextArea jtaR = new JTextArea("");
     private JScrollPane jspL = new JScrollPane();
@@ -21,21 +25,48 @@ public class encrypt extends JFrame {
     private JButton jbtnclose = new JButton("close");
     private ButtonGroup btnGroup = new ButtonGroup();
 
-    public static void main(String[] args) {
-        new encrypt().setVisible(true);
+    private JPanel jpl1 = new JPanel(new GridLayout(9,1,1,1));
+    private JPanel jpl2 = new JPanel(new GridLayout(1,1,1,1));
+    private JPanel jpl3 = new JPanel(new GridLayout(1,1,1,1));
+
+    private success mainframe;
+
+    public encrypt(success mainframe){
+        this.mainframe = mainframe;
+        init();
     }
-    public encrypt(){
-        z();
-    }
-    public void z(){
+    public void init(){
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setBounds(0,0,500,500);
-        cp.add(jtaL, BorderLayout.WEST);
-        cp.add(jpl1, BorderLayout.CENTER);
-        cp.add(jtaR, BorderLayout.EAST);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                mainframe.setVisible(true);
+
+            }
+        });
         cp = this.getContentPane();
-        cp.setLayout(new BorderLayout(1,1));
-        jpl1.add(jlbMethod); jpl1.add(jcomb); jpl1.add(jlbPW); jpl1.add(jtfPW); jpl1.add(jrbtn1); jpl1.add(jrbtn2); jpl1.add(jbtnrun); jpl1.add(jbtnclose);
-        jpl2.add(jtaL); jpl2.add(jspL);   jpl3.add(jtaR); jpl3.add(jspR);
+        cp.add(jpl2, BorderLayout.WEST);
+        cp.add(jpl1, BorderLayout.CENTER);
+        cp.add(jpl3, BorderLayout.EAST);
+        jtaL.setSize(150,500);
+        jtaL.setSize(150,500);
+//        cp.setLayout(new BorderLayout(1,1));
+
+        jpl1.add(jlbMethod);
+        jpl1.add(jcomb);
+        jpl1.add(jlbPW);
+        jpl1.add(jtfPW);
+        jpl1.add(jrbtn1);
+        jpl1.add(jrbtn2);
+        jpl1.add(jbtnrun);
+        jpl1.add(jbtnclose);
+
+        jpl2.add(jtaL);
+        jtaL.add(jspL);
+
+        jpl3.add(jtaR);
+        jtaR.add(jspR);
 
         btnGroup.add(jrbtn1);
         btnGroup.add(jrbtn2);
