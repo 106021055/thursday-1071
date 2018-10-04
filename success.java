6d299f6dae555a6b3b816674c796add727cae6a0
106021055 <32056108+106021055@users.noreply.github.com>
2018-10-04 11:38:20 +0800

    Add files via upload

--- success.java
+++ success.java
@@ -1,17 +1,46 @@
-//0927登入視窗的成功畫面
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class success extends JFrame{
+    private JMenuBar jmb = new JMenuBar();
+    private JMenu jmF = new JMenu("File");
+    private JMenu jmT = new JMenu("Tool");
+    private JMenu jmG = new JMenu("Game");
+    private JMenu jmH = new JMenu("Help");
+    private JMenuItem jmiopen = new JMenuItem("Open");
+    private JMenuItem jmiclose = new JMenuItem("Close");
+    private JMenuItem jmiexit = new JMenuItem("Exit");
+    private JMenuItem jmiooxx = new JMenuItem("OOXX");
+
     public static void main(String[] args) {
         new success().setVisible(true);
     }
-    private JLabel jlb = new JLabel("成功");
     public success(){
         q();
     }
     public void q(){
-        this.setBounds(500,500,100,100);
-        jlb.setBounds(100,50,100,100);
-        this.add(jlb);
+//        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setBounds(200,200,800,800);
+        this.setLayout(null);
+        this.setJMenuBar(jmb);
+        jmb.add(jmF); jmb.add(jmT); jmb.add(jmG); jmb.add(jmH);
+        jmF.add(jmiopen); jmF.add(jmiclose); jmF.add(jmiexit);
+        jmG.add(jmiooxx);
+
+         jmiooxx.addActionListener(new AbstractAction() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 OOXX w = new OOXX(success.this);
+                 w.setVisible(true);
+                 success.this.setVisible(false);
+             }
+         });
+
+         jmiexit.addActionListener(new AbstractAction() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 System.exit(0);
+             }
+         });
     }
 }
